3f994d73711440978bcd57dd60e43b5c8ca4528e
Byungwook <woook27@naver.com>
2023-01-06 14:37:12 +0900

    Chore: remove dependency on JUnit4

--- src/test/java/com/woookk/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/woookk/study/springboot/web/HelloControllerTest.java
@@ -1,16 +1,13 @@
 package com.woookk.study.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
--- src/test/java/com/woookk/study/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/woookk/study/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,7 +1,6 @@
 package com.woookk.study.springboot.web.dto;
 
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class HelloResponseDtoTest {
