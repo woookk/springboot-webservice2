9d59bbd7103f0b294bac62ec6f8de4f6487b11a1
Byungwook <woook27@naver.com>
2023-01-08 20:01:26 +0900

    Feat: create posts update page

--- src/main/java/com/woookk/study/springboot/web/controller/IndexController.java
+++ src/main/java/com/woookk/study/springboot/web/controller/IndexController.java
@@ -1,10 +1,12 @@
 package com.woookk.study.springboot.web.controller;
 
+import com.woookk.study.springboot.web.dto.posts.PostsResponseDto;
 import com.woookk.study.springboot.web.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
